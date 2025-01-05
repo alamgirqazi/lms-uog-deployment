166b490cb55b67d890166f25c5e8453776d5ae05
Alamgir Munir Qazi <alamgirmunirqazi@gmail.com>
2025-01-05 15:42:37 +0000

    refactoring

--- frontend-react/src/__tests__/App.test.tsx
+++ frontend-react/src/__tests__/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 import '@testing-library/jest-dom';
 
 test('renders login view by default', () => {
